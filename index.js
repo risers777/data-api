ba86886ee179ab3b6188c748e24abbb024e77c97
Seasparks <risers777@gmail.com>
2024-12-31 11:22:54 -0600

    Adding HMAC auth

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const { Sequelize, DataTypes } = require('sequelize');
 const helmet = require('helmet');
 const compression = require('compression');
 const ratelimit = require('express-rate-limit');
+const crypto = require('crypto');
+const HMAC_KEY = 'cupcakes';
+
 
 
 
@@ -11,9 +14,7 @@ const ratelimit = require('express-rate-limit');
 //     dialect: 'postgres'
 // });
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-   // host: DBServerName,
-    dialect: "postgres",   
-    port: 5432,
+    dialect: "postgres"
   });
 
 const SensorData = sequelize.define('sensor-data', {
@@ -43,14 +44,37 @@ app.use(compression());
 app.use(limiter);
 app.use(express.json()); 
 
+app.use((req, res, next) =>{
+    let key = req.query.key;
+    if(!key || key !== '12345'){
+        res.status(403).send();
+        return;
+    }
+    next();
+});
+
+
 app.get('/data', async (req, res) => {
-const allData = await SensorData.findAll();
+    let limit = req.query.limit || 5;
+    let offset = req.query.offset || 0;
+const allData = await SensorData.findAll({limit, offset});
 res.status(200).send(allData) 
 return;
 });
 
 app.post('/data', async (req, res) => { 
     let data = req.body; 
+    let hmac = req.headers['hmac']; // generated that in the REPL
+    let hmacExpected = crypto.createHmac('sha1', HMAC_KEY) // created a Hmac key through crypto library
+    .update(JSON.stringify(data))
+    .digest('hex');
+    let hmacEqual = crypto.timingSafeEqual(Buffer.from(hamc), Buffer.from(hmacExpected)); // verified if both are same
+    if(!hmacEqual) {
+        res.status(403).send('Bad hmac');
+        return;
+    }
+
+    console.log({ hmacEqual });
     const sensorData = await SensorData.create(data); 
     res.status(201).send(data); 
     return; 
